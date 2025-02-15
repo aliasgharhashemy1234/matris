bd65e05747d04f2405e8005b8a2fe8265e4c2633
aliasgharhashemy1234 <hashemyali155@gmail.com>
2025-02-16 01:33:59 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 
-int fibonacci(int n) {
-    if (n <= 0) {
-        return 0;
-    }
-    if (n == 1) {
-        return 1;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+struct Book {
+    string title;       // عنوان کتاب
+    string author;      // نویسنده
+    int year;           // سال انتشار
+    int pages;          // تعداد صفحات
+    double price;       // قیمت کتاب
+};
+
+// تابع برای نمایش اطلاعات یک کتاب
+void printBookInfo(const Book& book) {
+    cout << "Title: " << book.title << endl;
+    cout << "Author: " << book.author << endl;
+    cout << "Year: " << book.year << endl;
+    cout << "Pages: " << book.pages << endl;
+    cout << "Price: tuman" << book.price<<endl ;
+    cout << "--------------------------" << endl;
 }
 
 int main() {
-    int number;
-    cout << "n adad ra vared konid): ";
-    cin >> number;
 
-    if (number < 0) {
-        cout << "adad bayad bozorg tar ya mosavi sefr bashad !" << endl;
-    } else {
-        int result = fibonacci(number);
-        cout << "adad " << number << "n dar donbale barabar ast ba : " << result << endl;
-    }
+    Book book1 = {"buf kur ", "sadegh hedayat", 1315, 180, 30000};
+    Book book2 = {"samphoni mordegan", "abbas maeruphy", 1368, 328,50000};
+    Book book3 = {"soshon ", "simin daneshvar",1348, 281, 100000};
+
+
+    printBookInfo(book1);
+    printBookInfo(book2);
+    printBookInfo(book3);
 
     return 0;
 }
